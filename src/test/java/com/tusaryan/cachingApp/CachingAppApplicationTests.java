9ba9c0e19cc78c89a4ab6e88646662fb0ea4f797
Aryan Kumar <aryankumar79922@gmail.com>
2025-04-15 18:01:59 +0530

    initial commit

---
+++ src/test/java/com/tusaryan/cachingApp/CachingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusaryan.cachingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
